1a31ff8541246cf803b0088be79e7094d3750357
danclas <dan-vershinin2011@mail.ru>
2021-11-04 19:11:48 +0300

    Init commit

---
+++ bot.py
@@ -0,0 +1,109 @@
+import os
+import processing
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.dispatcher import FSMContext
+
+API_KEY = os.getenv('API_KEY')
+
+
+class ImageProc(StatesGroup):
+    choosing = State()
+    return_img = State()
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(API_KEY)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'], state=None)
+async def start_message(message: types.Message):
+    await message.answer(
+        'Привет \U0001F44B ! Я могу помочь тебе обработать твоё изображение! Присылай изображение и выбирай фильтр.\n\n'
+        'Доступные фильтры: \n'
+        '1. Серое изображение.\n'
+        '2. Заблюренное изображение.\n'
+        '3. Добавить яркости.\n'
+        '4. Убавить яркости.\n')
+
+
+@dp.message_handler(commands=['help'], state=None)
+async def start_message(message: types.Message):
+    await message.answer('Присылай изображение и выбирай фильтр!\n\n'
+                         'Доступные фильтры: \n'
+                         '1. Серое изображение.\n'
+                         '2. Заблюренное изображение.\n'
+                         '3. Добавить яркости.\n'
+                         '4. Убавить яркости.\n')
+
+
+@dp.message_handler(commands=['new'], state=None)
+async def new_message(message: types.Message):
+    await message.answer('Можете присылать фото', reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(content_types=['photo'], state=None)
+async def get_img(message: types.Message):
+    await message.photo[-1].download(destination_file='photos/photo_{}.jpg'.format(message.from_user.id))
+    # await message.answer('da',reply_markup=ReplyKeyboardRemove())
+    await ImageProc.choosing.set()
+    await choosing_filter(message)
+
+
+@dp.message_handler(state=ImageProc.choosing)
+async def choosing_filter(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    button_gray = KeyboardButton('Серое изображение')
+    button_blur = KeyboardButton('Заблюренное изображение')
+    button_add = KeyboardButton('Добавить яркости')
+    button_sub = KeyboardButton('Убавить яркости')
+    # buttons = ['Серое изображение', 'Заблюренное изображение', 'Добавить яркости', 'Убавить яркости']
+    # keyboard.add(*buttons)
+    keyboard.row(button_gray, button_blur)
+    keyboard.row(button_add, button_sub)
+    await message.answer('Выберите фильтр', reply_markup=keyboard)
+    await ImageProc.return_img.set()
+
+
+@dp.message_handler(content_types=['any'], state=ImageProc.return_img)
+async def send_img(message: types.Message, state: FSMContext):
+    user_id = message.from_user.id
+    load_img = False
+
+    if message.text == 'Серое изображение':
+        img = processing.read_img(user_id)
+        processing.save_img(processing.get_grayscale_img(img), user_id)
+        load_img = True
+    elif message.text == 'Заблюренное изображение':
+        img = processing.read_img(user_id)
+        processing.save_img(processing.get_blur_img(img), user_id)
+        load_img = True
+    elif message.text == 'Добавить яркости':
+        img = processing.read_img(user_id)
+        processing.save_img(processing.get_add_bright_img(img), user_id)
+        load_img = True
+    elif message.text == 'Убавить яркости':
+        img = processing.read_img(user_id)
+        processing.save_img(processing.get_sub_bright_img(img), user_id)
+        load_img = True
+
+    if load_img == True:
+        result = types.InputFile('photos/result_{}.jpg'.format(user_id))
+
+        await bot.send_photo(message.from_user.id, result, reply_markup=ReplyKeyboardRemove())
+        os.remove('photos/photo_{}.jpg'.format(user_id))
+        os.remove('photos/result_{}.jpg'.format(user_id))
+        await state.finish()
+    else:
+        await message.answer('Если что-то пошло не так, наберите /new')
+        await state.finish()
+
+
+# run long-polling
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ processing.py
@@ -0,0 +1,71 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def read_img(user_id):
+    return cv2.imread('photos/photo_{}.jpg'.format(user_id))
+
+
+def save_img(img, user_id):
+    cv2.imwrite('photos/result_{}.jpg'.format(user_id), img)
+
+
+def get_grayscale_img(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+def get_black_white_img(img):
+    (thresh, im_bw) = cv2.threshold(get_grayscale_img(img), 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    return im_bw
+
+
+def get_blur_img(img):
+    return cv2.medianBlur(img, 15)
+
+
+def get_add_bright_img(img):
+    increase = 30
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    v = hsv[:, :, 2]
+    v = np.where(v <= 255 - increase, v + increase, 255)
+    hsv[:, :, 2] = v
+
+    result = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return result
+
+
+def get_sub_bright_img(img):
+    increase = 30
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    v = hsv[:, :, 2]
+    v = np.where(v > increase, v - increase, 0)
+    hsv[:, :, 2] = v
+
+    result = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return result
+
+
+if __name__ == '__main__':
+    img = cv2.imread('wallhaven-28ovmy.jpg')
+
+    fig, axs = plt.subplots()
+    axs.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+    fig, axs = plt.subplots()
+    axs.imshow(get_grayscale_img(img), cmap='gray')
+
+    fig, axs = plt.subplots()
+    axs.imshow(get_black_white_img(img), cmap='gray')
+
+    fig, axs = plt.subplots()
+    axs.imshow(cv2.cvtColor(get_blur_img(img), cv2.COLOR_BGR2RGB))
+
+    fig, axs = plt.subplots()
+    axs.imshow(cv2.cvtColor(get_sub_bright_img(img), cv2.COLOR_BGR2RGB))
+
+    plt.show()
+
+    cv2.imwrite('saveim.jpg', get_sub_bright_img(img))
